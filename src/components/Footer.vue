edeca518a59095712e2e9a59b3a6bad182142490
Davide Bontempelli <davide.bonte@gmail.com>
2018-12-30 15:31:44 +0100

    add basic header and footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Footer TBD</h1>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Header TBD</h1>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
