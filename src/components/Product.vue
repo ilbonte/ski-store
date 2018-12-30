7f4e1e9166d065fd602843a8e380255cd37ad7c1
Davide Bontempelli <davide.bonte@gmail.com>
2018-12-30 14:24:07 +0100

    create ski store

---
+++ src/components/Product.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>This pair of ski: {{name}}</h1>
+</template>
+
+<script>
+export default {
+  name: "Product",
+  props: ["name"]
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/SkiStore.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Welcome to the best ski store</h1>
+    <Product name="ski trab"/>
+    <Product name="fisher"/>
+  </div>
+</template>
+
+<script>
+import Product from "./Product.vue";
+
+export default {
+  name: "SkiStore",
+  components: {
+    Product
+  }
+};
+</script>
+
+<style>
+</style>
