99dc79a245df24d4ef04ea437a5bee3d1712f00e
Davide Bontempelli <davide.bonte@gmail.com>
2019-01-23 14:29:08 +0100

    add form

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,39 @@
 <template>
-  <h1>Footer TBD</h1>
+  <div>
+    <h1>Footer TBD</h1>
+    <form name="buySki" method="POST" data-netlify="true">
+      <p>
+        <label>
+          Your Name:
+          <input type="text" name="name">
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email:
+          <input type="email" name="email">
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Role:
+          <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select>
+        </label>
+      </p>
+      <p>
+        <label>
+          Message:
+          <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </div>
 </template>
 
 <script>
